a972f7b30dbbac9f01c33b1313104c13f584f768
Manolo Pina <contato@manolopina.com.br>
2017-12-16 20:43:59 -0200

    Primeiro commit

---
+++ src/app/browser-event-experiments/browser-event-experiments.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'browser-event-experiments',
+  templateUrl: './browser-event-experiments.component.html',
+  styleUrls: ['./browser-event-experiments.component.css']
+})
+export class BrowserEventExperimentsComponent implements OnInit {
+
+  hoverSection:HTMLElement;
+
+  constructor() { }
+
+  ngOnInit() {
+    
+    this.hoverSection = document.getElementById("hover");
+
+    this.hoverSection.addEventListener("mousemove", this.onMouseMove);
+
+  }
+
+  unsubscibre() {
+    console.log('Called unsubscribe()');
+    this.hoverSection.removeEventListener("mousemove", this.onMouseMove);
+  }
+
+
+  onMouseMove(ev: MouseEvent) {
+    console.log(ev);
+  }
+
+}
